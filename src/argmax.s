475cc85004e1e515cf35d8675338e2a0b2b13767
Felipe <26683783+feslima@users.noreply.github.com>
2024-06-13 14:59:45 -0300

    feat: task 2 - argmax implementation

--- src/argmax.s
+++ src/argmax.s
@@ -19,28 +19,55 @@ argmax:
     blt a1, t0, error_empty_vector
 
     # Prologue
+    addi sp sp -20
+    sw ra 16(sp)
+    sw s3 12(sp)
+    sw s2 8(sp)
+    sw s1 4(sp)
+    sw s0 0(sp)
+
+    mv s0, a0
+    mv s1, a1
+
+    li t0, 0        # int i = 0
+    li t1, 1
+    srli t1, t1, 31 # t1 = current max value
+    mv s2, t0       # s2 = index of current max
+    
+    j loop_start
 
 error_empty_vector:
     li a1 7
     jal exit2
 
 loop_start:
+    bge t0, s1, loop_end
 
+    slli t2, t0, 2 # stride of 4 bytes
+    add s3, s0, t2 # &(arr[i])
+    lw t3, 0(s3)   # *(arr[i])
+    
+    sub t4, t3, t1 # t4 = current_max - *(arr[i])
+    bgtz t4, update_max
+    after_update_max:
+    
+    addi t0, t0, 1 # i++
+    j loop_start
 
-
-
-
-
-
-
-loop_continue:
-
-
+update_max:
+    mv t1, t3
+    mv s2, t0
+    j after_update_max
 
 loop_end:
-    
+    mv a0, s2
 
     # Epilogue
-
+    lw s0 0(sp)
+    lw s1 4(sp)
+    lw s2 8(sp)
+    lw s3 12(sp)
+    lw ra 16(sp)
+    addi sp sp 20
 
     ret
--- tests/assembly/test_argmax.s
+++ tests/assembly/test_argmax.s
@@ -2,7 +2,9 @@
 .import ../../src/utils.s
 
 .data
-v0: .word 3 -42 432 7 -5 6 5 -114 2 # MAKE CHANGES HERE
+v0: .word 3 -42 432 7 -5 6 5 -114 2
+v1: .word 3 -42 7 432 -500 6 5 -114 2 # ensure magnitude of large negative is ignored
+v2: .word 3 432 -42 7 -5 6 5 -114 432 # mutiple maxima case
 
 .text
 main:
@@ -10,11 +12,35 @@ main:
     la s0 v0
     
     # Set length of v0
-    addi s1 x0 9 # MAKE CHANGES HERE
+    addi s1 x0 9
 
-    # Call argmax
+    # Call argmax of v0
     mv a0 s0
     mv a1 s1
+    jal ra argmax
+
+    # Print the output of argmax
+    mv a1 a0
+    jal ra print_int
+
+    # Print newline
+    li a1 '\n'
+    jal ra print_char
+
+    # Call argmax of v1
+    la a0 v1
+    jal ra argmax
+
+    # Print the output of argmax
+    mv a1 a0
+    jal ra print_int
+
+    # Print newline
+    li a1 '\n'
+    jal ra print_char
+
+    # Call argmax of v2
+    la a0 v2
     jal ra argmax
 
     # Print the output of argmax
