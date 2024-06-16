95f656dde4ba9633c1899d697cc8f5fb3ec994ae
Felipe <26683783+feslima@users.noreply.github.com>
2024-06-16 18:44:41 -0300

    feat: (B) task 2 - write matrix implementation

--- src/write_matrix.s
+++ src/write_matrix.s
@@ -27,16 +27,81 @@
 write_matrix:
 
     # Prologue
+    addi sp, sp, -28
+    sw ra, 24(sp)
+    sw s5, 20(sp)
+    sw s4, 16(sp)
+    sw s3, 12(sp)
+    sw s2, 8(sp)
+    sw s1, 4(sp)
+    sw s0, 0(sp)
+    
+    mv s0, a0
+    mv s1, a1
+    mv s2, a2
+    mv s3, a3
 
+    # open the file
+    mv a1, s0
+    li a2, 1 # write only permission
+    jal fopen
+    li t0, -1
+    beq, a0, t0, error_open
+    mv s4, a0 # s4 =  file descriptor
+    
+    # write file contents
+    mv a1, s4
+    
+    # write rows and columns
+    addi sp, sp, -8
+    sw s3, 4(sp)
+    sw s2, 0(sp)
+    mv a2, sp
+    li a3, 2 # two elements
 
+    li a4, 4 # 4 bytes per number
+    jal fwrite
+    bne a0, a3, error_write 
+    addi sp, sp, 8 # deallocate used space
 
+    # write the matrix elements
+    mv a1, s4
+    mv a2, s1
+    mul s5, s2, s3 # s5 = number of elements to be written 
+    mv a3, s5
 
+    jal fwrite
+    bne a0, s5, error_write 
+    mv a1, s4
+    jal fflush
+    bnez a0, error_write
 
-
-
-
+    # close file
+    mv a1, s4
+    jal fclose
+    li t0, -1
+    beq a0, t0, error_close
 
     # Epilogue
-
+    lw s0, 0(sp)
+    lw s1, 4(sp)
+    lw s2, 8(sp)
+    lw s3, 12(sp)
+    lw s4, 16(sp)
+    lw s5, 20(sp)
+    lw ra, 24(sp)
+    addi sp, sp, -28
 
     ret
+
+error_open:
+    li a1, 53
+    jal exit2
+
+error_write:
+    li a1, 54
+    jal exit2
+
+error_close:
+    li a1, 55
+    jal exit2
--- tests/assembly/test_write_matrix.s
+++ tests/assembly/test_write_matrix.s
@@ -2,12 +2,21 @@
 .import ../../src/utils.s
 
 .data
-m0: .word 1, 2, 3, 4, 5, 6, 7, 8, 9 # MAKE CHANGES HERE TO TEST DIFFERENT MATRICES
+m0: .word 1, 2, 3, 4, 5, 6, 7, 8, 9
+rows: .word 3
+cols: .word 3
 file_path: .asciiz "outputs/test_write_matrix/student_write_outputs.bin"
 
 .text
 main:
     # Write the matrix to a file
-
+    la a0, file_path
+    la a1, m0
+    la a2, rows
+    lw a2, 0(a2)
+    la a3, rows
+    lw a3, 0(a3)
+    jal write_matrix
 
     # Exit the program
+    jal exit
